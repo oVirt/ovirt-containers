0d983cb0651af9d28777c8ed39159844c9409d7a
Yaniv Bronhaim <ybronhei@redhat.com>
2017-03-23 17:09:12 +0200

    Fix network issue in node deployment
    - make node-deployment a list as engine deployment
    - add modprobe commands to deploy script
    - mount host's /etc/sysconfig/network-scripts
    - persist /var/run/vdsm and /var/lib/vdsm
    
    Change-Id: I76c85599b928215186ccb7033b2be0b4b1d4ff57
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- image-specifications/node/scripts/deploy.sh
+++ image-specifications/node/scripts/deploy.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-# Waiting until vdsmd is up.. better to check service status before running
-sleep 10
+modprobe bonding
+modprobe 8021q
 
 vdsm-tool configure --force
+service vdsmd restart
+
+sleep 5
 
 # setting link name - vdsm recognize veth_* prefix as veth type device
 ip link set eth0 down
 ip link set eth0 name veth_name0
 ip link set veth_name0 up
+
+sleep 5
 ip route add default via $CLUSTER_GATEWAY dev veth_name0
 #
 
