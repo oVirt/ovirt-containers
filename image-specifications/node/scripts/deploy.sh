c7c85eebb7fec181ca042f5352f56a1e3f2dc078
Yaniv Bronhaim <ybronhei@redhat.com>
2017-03-25 18:37:06 +0300

    node image: raise timeout before setting route
    interface up take more than 5 seconds, cause ip route command to fail
    
    Change-Id: I9f9caf3cf4d51cbbc068e6af9e2d3f4c04140fa7
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- image-specifications/node/scripts/deploy.sh
+++ image-specifications/node/scripts/deploy.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
-
 modprobe bonding
 modprobe 8021q
 
 vdsm-tool configure --force
 service vdsmd restart
 
-sleep 5
-
 # setting link name - vdsm recognize veth_* prefix as veth type device
 ip link set eth0 down
 ip link set eth0 name veth_name0
 ip link set veth_name0 up
-
-sleep 5
+sleep 10
 ip route add default via $CLUSTER_GATEWAY dev veth_name0
-#
-
 python /root/add_network.py
 
 # ovirt registration flow.
