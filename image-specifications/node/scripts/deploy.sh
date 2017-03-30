bdaddb560771107b64cd10084cd1d6b26bc2b3e1
Simone Tiraboschi <stirabos@redhat.com>
2017-03-30 10:20:53 +0200

    node: hostPort and host-deploy related fixes
    - use hostPort instead of a specific node service
    - create /etc/ovirt-container-node-release to have
    host-deploy detecting the container as a special node
    - force the right permissions on /dev/kvm mounted from
    the host
    
    Change-Id: I037c8c9a812e63e4f894b4f054ff4e81c213fd1b
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- image-specifications/node/scripts/deploy.sh
+++ image-specifications/node/scripts/deploy.sh
@@ -2,6 +2,10 @@
 modprobe bonding
 modprobe 8021q
 
+mkdir -p /var/log/vdsm
+mkdir -p /var/log/ovirt-imageio-daemon
+chown vdsm:kvm /var/log/vdsm /var/log/ovirt-imageio-daemon
+
 vdsm-tool configure --force
 service vdsmd restart
 
@@ -13,7 +17,12 @@ sleep 10
 ip route add default via $CLUSTER_GATEWAY dev veth_name0
 python /root/add_network.py
 
+# /dev is mounted from the host so we have to fix permissions at runtime
+chown root:kvm /dev/kvm
+chmod 0660 /dev/kvm
+
 # ovirt registration flow.
 MYNAME=$(hostname -f)
-SSHPORT=222
-vdsm-tool register --engine-fqdn $ENGINE_FQDN --check-fqdn false --node-address $MYADDR --node-name $MY_NODE_NAME --ssh-port $SSHPORT
+SSHPORT=22222
+vdsm-tool register --engine-fqdn $ENGINE_FQDN --check-fqdn false \
+	--node-name $MYNAME --node-address $MY_NODE_NAME --ssh-port $SSHPORT
