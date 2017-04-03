43ce1aa3a63233045e1745d7852cf55af07b255a
Yaniv Bronhaim <ybronhei@redhat.com>
2017-04-03 18:19:40 +0300

    node script: removing env vars usage
    Change-Id: I2ab65ee8b60c728c2bba50eba2b00cad4485235f
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- image-specifications/node/scripts/add_network.py
+++ image-specifications/node/scripts/add_network.py
@@ -1,13 +1,12 @@
 #!/bin/python
 from vdsm import vdscli
-import os
 import socket
 
 c = vdscli.connect()
 network_attrs = {'nic': 'veth_name0',
                  'ipaddr': socket.gethostbyname(socket.gethostname()),
                  'netmask': '255.240.0.0',
-                 'gateway': os.environ['CLUSTER_GATEWAY'],
+                 'gateway': '172.17.0.1',
                  'defaultRoute': True,
                  'bridged': True}
 
--- image-specifications/node/scripts/deploy.sh
+++ image-specifications/node/scripts/deploy.sh
@@ -8,7 +8,6 @@ mkdir -p /var/log/ovirt-imageio-daemon
 chown vdsm:kvm /var/log/vdsm /var/log/ovirt-imageio-daemon
 
 vdsm-tool configure --force
-service vdsmd restart
 
 sleep 5
 
@@ -17,18 +16,11 @@ ip link set eth0 down
 ip link set eth0 name veth_name0
 ip link set veth_name0 up
 
-sleep 5
-ip route add default via $CLUSTER_GATEWAY dev veth_name0
-#
-
+sleep 10
+ip route add default via 172.17.0.1 dev veth_name0
+service vdsmd restart
 python /root/add_network.py
 
 # /dev is mounted from the host so we have to fix permissions at runtime
 chown root:kvm /dev/kvm
 chmod 0660 /dev/kvm
-
-# ovirt registration flow.
-MYNAME=$(hostname -f)
-SSHPORT=22222
-vdsm-tool register --engine-fqdn $ENGINE_FQDN --check-fqdn false \
-	--node-name $MYNAME --node-address $MY_NODE_NAME --ssh-port $SSHPORT
