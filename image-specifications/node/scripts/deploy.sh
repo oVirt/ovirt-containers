7e8bd234ce995179bfa638a205443633a04374f6
Yaniv Bronhaim <ybronhei@redhat.com>
2017-04-03 18:19:40 +0300

    node script: removing env vars usage
    Change-Id: I2ab65ee8b60c728c2bba50eba2b00cad4485235f
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- image-specifications/node/scripts/deploy.sh
+++ image-specifications/node/scripts/deploy.sh
@@ -7,22 +7,16 @@ mkdir -p /var/log/ovirt-imageio-daemon
 chown vdsm:kvm /var/log/vdsm /var/log/ovirt-imageio-daemon
 
 vdsm-tool configure --force
-service vdsmd restart
 
 # setting link name - vdsm recognize veth_* prefix as veth type device
 ip link set eth0 down
 ip link set eth0 name veth_name0
 ip link set veth_name0 up
 sleep 10
-ip route add default via $CLUSTER_GATEWAY dev veth_name0
+ip route add default via 172.17.0.1 dev veth_name0
+service vdsmd restart
 python /root/add_network.py
 
 # /dev is mounted from the host so we have to fix permissions at runtime
 chown root:kvm /dev/kvm
 chmod 0660 /dev/kvm
-
-# ovirt registration flow.
-MYNAME=$(hostname -f)
-SSHPORT=22222
-vdsm-tool register --engine-fqdn $ENGINE_FQDN --check-fqdn false \
-	--node-name $MYNAME --node-address $MY_NODE_NAME --ssh-port $SSHPORT
