619c387f189785e3007fcc80afa13607bf7ed74e
Juan Hernandez <juan.hernandez@redhat.com>
2017-04-11 23:13:17 +0200

    Add 'autovacuum' parameters to the PostgreSQL image
    Recent versions of the engine need to have the following auto-vacuum
    configuration parameters:
    
      autovacuum_vacuum_scale_factor = 0.01
      autovacuum_analyze_scale_factor = 0.075
      autovacuum_max_workers = 6
      maintenance_work_mem = 65536
    
    Without them the setup tool fails during the start of the engine container.
    
    This patch modifies the 'ovirt-db-config.sh' file so that those parameter are
    automatically added to the 'postgresql.conf' file during the start of the
    PostgreSQL container.
    
    Change-Id: I90924a3326d478611f7f2962e2a641968da86023
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    (cherry picked from commit 5d6764853dce66adeac80336bd14e7d3105108ce)

--- image-specifications/ovirt-postgres/ovirt-db-config.sh
+++ image-specifications/ovirt-postgres/ovirt-db-config.sh
@@ -1,2 +1,19 @@
-#!/bin/bash
-sed -i $PGDATA/postgresql.conf -e "s/^max_connections .*$/max_connections = $MAX_CONNECTIONS/"
+#!/bin/bash -ex
+
+# The engine needs a 'max_connections' parameter larger than the default used
+# by PostgreSQL, at most 150. As this parameter already has a value in the
+# default 'postgresql.conf' file, we need to edit the file to fix it.
+sed -i \
+  -e "s/^\(max_connections\)\\s*=.*$/\1 = ${MAX_CONNECTIONS}/" \
+  "${PGDATA}/postgresql.conf"
+
+# Recent versions of the engine also need custom auto-vacuum parameters. The
+# setup tool checks them, and fails the start of the engine if they aren't set.
+# These are commented out by default, so we just need to append them to the
+# file.
+cat >> "${PGDATA}/postgresql.conf" <<.
+autovacuum_vacuum_scale_factor = 0.01
+autovacuum_analyze_scale_factor = 0.075
+autovacuum_max_workers = 6
+maintenance_work_mem = 65536
+.
