e7006ad11f08f517d0cfb1628341ae72a48cd9ea
Juan Hernandez <juan.hernandez@redhat.com>
2017-04-11 20:31:53 +0200

    Use 'ovirt' prefix and 'master' version
    This patch changes the names of the generated images so that they all
    use the 'ovirt' prefix and the 'master' version.
    
    Note that according to the OVIRT-758 issue referenced below, the
    official oVirt docker hub account is 'ovirt', not 'ovirtorg'.
    
    Change-Id: I9f048a49878c09d145472f327d5a98611e6751f3
    Related-To: https://ovirt-jira.atlassian.net/browse/OVIRT-758
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -2,12 +2,12 @@
 [[ -d exported-artifacts ]] \
 || mkdir -p exported-artifacts
 
-IMG_PREFIX="docker.io/bronhaim/"
+IMG_PREFIX="ovirt/"
 IMAGES_PATH="./image-specifications"
 
 # we can use here "git describe --all" if we want to keep image history
 # but then we need to change openshift deployment to use this tag
-TAGV="4.1"
+TAGV="master"
 
 function clean_up {
     docker images -f dangling=true -q | xargs -r docker rmi -f
