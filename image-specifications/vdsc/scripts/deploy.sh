7b00a65b36382d812f16dc58acaee45d890fdaa0
Simone Tiraboschi <stirabos@redhat.com>
2017-05-11 16:34:45 +0200

    vdsc: auto-registration flow
    Reintroduce auto-registration flow on VDSC container.
    Get the external engine address fetching the CN field
    on the TLS cert got connecting the engine on its service
    address.
    Get the node address using kubernetes Environment Variables
    since status.hostIP will be available within downward volume
    API only with kubernetes 1.7 (see
    https://github.com/kubernetes/kubernetes/pull/42717 ).
    Since the registration command it's running in a
    systemd unit we have to get its env directly from
    /proc/1/environ
    
    Change-Id: I4ff7b7999d089aa581b44bf031f62f10fbbaa3cb
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- image-specifications/vdsc/scripts/deploy.sh
+++ image-specifications/vdsc/scripts/deploy.sh
@@ -29,3 +29,17 @@ service vdsmd restart
 sleep 5
 
 python /root/add_network.py
+
+# ovirt registration flow.
+declare $(xargs -n 1 -0 < /proc/1/environ | grep MY_NODE_NAME)
+MYNAME=$(hostname -f)
+SSHPORT=22222
+ENGINE_SERVICE=ovirt-engine.ovirt.svc
+ENGINE_HTTPS_PORT=443
+ENGINE_FQDN=$(echo "Q" | openssl s_client -connect $ENGINE_SERVICE:$ENGINE_HTTPS_PORT 2>&1 | grep "subject=" | sed 's/^.*CN=//g')
+vdsm-tool register --engine-fqdn $ENGINE_FQDN \
+                   --engine-https-port $ENGINE_HTTPS_PORT \
+                   --check-fqdn false \
+                   --node-name $MYNAME \
+                   --node-address $MY_NODE_NAME \
+                   --ssh-port $SSHPORT
